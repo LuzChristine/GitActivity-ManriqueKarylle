f02209d2e4c558e8f46ae47d2a07848e9b5def99
ManriqueKarylle <manriquekarylle891@gmail.com>
2022-03-13 14:25:17 +0800

    My Educational Background added

--- git-python-manrique.py
+++ git-python-manrique.py
@@ -1,3 +1,6 @@
-print ("Karylle M. Manrique);
+print ("Karylle M. Manrique");
 print ("Poras Boac Marinduque");
 print ("June 28, 2001");
+print ("Marinduque State College");
+print ("BSIT");
+print ("2D");
